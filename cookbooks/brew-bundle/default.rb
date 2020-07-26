42364968bdeb5794823366f051352ce3d1998ea6
Daisuke HIGASHI <me@lulichn.net>
2020-07-26 11:37:44 +0900

    update

--- cookbooks/brew-bundle/default.rb
+++ cookbooks/brew-bundle/default.rb
@@ -1,12 +1,6 @@
-%w[
-  base
-  apps
-  brewfile
-  multimedia
-].each do |file|
-  execute 'brew bundle' do
-    command <<-"EOF"
-      brew bundle --file=#{File.join(File.expand_path('..', __FILE__), 'files', file)}
-    EOF
-  end
+
+execute 'brew bundle' do
+  command <<-"EOF"
+    brew bundle --file=#{File.join(File.expand_path('..', __FILE__), 'files', 'Brewfile')}
+  EOF
 end
